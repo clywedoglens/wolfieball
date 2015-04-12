9217714a4c9d50def8be4172d05227c24d6e903b
George <George@192.168.1.127>
2015-04-12 19:59:20 -0400

    added errorhandler and messagedialog. will continue to work on startup constants and property types

--- src/wb/WolfieBall.java
+++ src/wb/WolfieBall.java
@@ -5,6 +5,7 @@
  */
 package wb;
 
+import java.io.IOException;
 import java.util.Locale;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
@@ -15,7 +16,16 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import wb.error.ErrorHandler;
+import wb.file.JsonPlayerFileManager;
 import wb.gui.WB_GUI;
+import properties_manager.PropertiesManager;
+import static wb.WB_StartupConstants.PATH_BASE;
+import static wb.WB_StartupConstants.PATH_DATA;
+import static wb.WB_StartupConstants.PATH_SITES;
+import static wb.WB_StartupConstants.PROPERTIES_FILE_NAME;
+import static wb.WB_StartupConstants.PROPERTIES_SCHEMA_FILE_NAME;
+import wb.file.DraftSiteExporter;
+import xml_utilities.InvalidXMLFileFormatException;
 
 /**
  *
@@ -28,7 +38,7 @@ public class WolfieBall extends Application {
     @Override
     public void start(Stage primaryStage) {
         //GIVE THE PRIMARY STAGE TO OUR ERROR HANDLER
-        ErrorHandler eH = ErrorHander.getErrorHandler();
+        ErrorHandler eH = ErrorHandler.getErrorHandler();
         eH.initMessageDialog(primaryStage);
         
         //LOAD APP SETTINGS
@@ -37,14 +47,52 @@ public class WolfieBall extends Application {
             PropertiesManager props = PropertiesManager.getPropertiesManager();
             String appTitle = props.getProperty(PROP_APP_TITLE);
             try {
-                               
+                 
+                //WE WILL READ AND SAVE OUR PLAYER DATA USING THE JSON FILE
+                //FORMATER SO THIS OBJECT WILL HANDLE THAT
+                JsonPlayerFileManager jsonFileManager = new JsonPlayerFileManager();
                 
+                //THIS EXPORTS THE DRAFT RESULTS INTO A WEB PAGE
+                DraftSiteExporter exporter = new DraftSiteExporter(PATH_BASE, PATH_SITES);
+                
+                //THIS WILL LOAD THE DATA FROM JSON FILES
+                
+                //INTILIAZE GUI STUFF
+                gui = new WB_GUI(primaryStage);
+                gui.getPlayerFileManager(jsonFileManager);
+                gui.setSiteExporter(exporter);
+                
+                //CONSTRUCT THE DATA MANAGER AND GIVE IT TO THE GUI
+                PlayerDataManager dataManager = new PlayerDataManager(gui);
+                gui.setDataManager(dataManager);
+                
+                //START THE WINDOW THAT THE USER SEES
+                gui.initGUI(appTitle,);
+            }
+            catch(IOException ioe) {
+                eH = ErrorHandler.getErrorHandler();
+                eH.handlePropertiesFileError();
+            }
+               
             }
         }
+    
+    public boolean loadProperties() {
+        try {
+            //LOAD SETTINGS FOR STARTING THE APP
+            PropertiesManager props = PropertiesManager.getPropertiesManager();
+            props.addProperty(PropertiesManager.DATA_PATH_PROPERTY, PATH_DATA);
+            props.loadProperties(PROPERTIES_FILE_NAME, PROPERTIES_SCHEMA_FILE_NAME);
+            return true;
+        } catch(InvalidXMLFileFormatException ixmlffe) {
+            //SOMETHING WENT WRONG INITIALIZING THE XML FILE
+            ErrorHandler eH = ErrorHandler.getErrorHandler();
+            eH.handlePropertiesFileError();
+            return false;
+        }
     }
-
     /**
-     * @param args the command line arguments
+     * This is where the program execution begins.
      */
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
