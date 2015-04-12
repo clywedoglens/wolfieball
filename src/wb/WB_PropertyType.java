6fa8adb5aa10ce553de5caed649d1a3ec7f19d9e
George <George@192.168.1.127>
2015-04-12 01:20:02 -0400

    added startup/property values

---
+++ src/wb/WB_PropertyType.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wb;
+
+/**
+ *
+ * @author George
+ */
+public enum WB_PropertyType {
+    //LOADED FROM properties.xml
+    
+    //APPLICATION ICONS
+    //BASIC SCREEN CONTROL ICONS
+    NEW_DRAFT_ICON,
+    LOAD_DRAFT_ICON,
+    SAVE_DRAFT_ICON,
+    EXIT_ICON,
+    
+    //DRAFT SCREEN CONTROL ICONS
+    ADD_PLAYER_ICON,
+    REMOVE_PLAYER_ICON,
+    
+    //TEAMS SCREEN CONTROL ICONS
+    ADD_TEAM_ICON,
+    REMOVE_TEAM_ICON,
+    
+    
+    //DRAFT SCREEN CONTROL ICONS
+    DRAFT_PLAYER_ICON,
+    AUTO_DRAFT_ICON,
+    PAUSE_DRAFT_ICON,
+    
+    //APPLICATION TOOLTIPS FOR BUTTONS
+    NEW_DRAFT_TOOLTIP,
+    LOAD_DRAFT_TOOLTIP,
+    SAVE_DRAFT_TOOLTIP,
+    DELETE_TOOLTIP,
+    EXIT_TOOLTIP,
+    ADD_PLAYER_TOOLTIP,
+    REMOVE_PLAYER_TOOLTIP,
+    ADD_TEAM_TOOLTIP,
+    REMOVE_TEAM_TOOLTIP,
+    DRAFT_PLAYER_TOOLTIP,
+    AUTODRAFT_TOOLTIP,
+    PAUSE_DRAFT_TOOLTIP,
+    
+    WOLFIEBALL_TEMP_LABEL,
+    
+    //PLAYER POSITION LABELS
+    PLAYER_AVAILABLE_LABEL,
+    PLAYER_SEARCH_LABEL,
+    PLAYER_CATCHER_LABEL,
+    PLAYER_PITCHER_LABEL,
+    PLAYER_FIRST_LABEL,
+    PLAYER_SECOND_LABEL,
+    PLAYER_THIRD_LABEL,
+    PLAYER_SHORTSTOP_LABEL,
+    PLAYER_MIDDLE_LABEL,
+    PLAYER_OUTFIELD_LABEL
+    
+    
+    
+    
+}
--- src/wb/WolfieBall.java
+++ src/wb/WolfieBall.java
@@ -5,46 +5,49 @@
  */
 package wb;
 
+import java.util.Locale;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import wb.error.ErrorHandler;
+import wb.gui.WB_GUI;
 
 /**
  *
  * @author George
  */
 public class WolfieBall extends Application {
-    
+        //THIS IS THE FULL USER INTERFACE, WHICH 
+        //WILL BE INITIALIZED AFTER THE PROPERTIES FILE IS LOAD
+        WB_GUI gui;
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+        //GIVE THE PRIMARY STAGE TO OUR ERROR HANDLER
+        ErrorHandler eH = ErrorHander.getErrorHandler();
+        eH.initMessageDialog(primaryStage);
+        
+        //LOAD APP SETTINGS
+        boolean success = loadProperties();
+        if(success) {
+            PropertiesManager props = PropertiesManager.getPropertiesManager();
+            String appTitle = props.getProperty(PROP_APP_TITLE);
+            try {
+                               
+                
             }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        }
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         launch(args);
     }
     
