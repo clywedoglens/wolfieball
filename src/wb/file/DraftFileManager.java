36d456f3fc7a9df0e3873308eee05718059287eb
George <George@192.168.1.127>
2015-04-18 20:44:08 -0400

    completed hitters and pitchers java files

--- src/wb/file/DraftFileManager.java
+++ src/wb/file/DraftFileManager.java
@@ -5,10 +5,22 @@
  */
 package wb.file;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import wb.data.Draft;
+import wb.data.Team;
+
 /**
  *
  * @author George
  */
-public class DraftFileManager {
+public interface DraftFileManager {
+    public void                 saveDraft(Draft draftToSave) throws IOException;
+    public void                 loadDraft(Draft draftToLoad, String coursePath) throws IOException;
+    public void                 saveTeams(List<Object> Teams, String filePath) throws IOException;
+    public ArrayList<String>    loadTeams(String filePath) throws IOException;
+    public Team                 loadTeam(String filePath) throws IOException;
+    
     
 }
