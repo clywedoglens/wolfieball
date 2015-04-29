9656dc25feba1241fd2156fdb106eab92cdf9448
George <George@192.168.1.117>
2015-04-29 00:30:23 -0400

    finished setting up the teams page. will add functionality

--- src/wb/WB_StartupConstants.java
+++ src/wb/WB_StartupConstants.java
@@ -18,6 +18,7 @@ public class WB_StartupConstants {
     public static final String PATH_DATA = "./data/";
     public static final String PATH_DRAFTS = PATH_DATA + "drafts/";
     public static final String PATH_PLAYERS = PATH_DATA + "players/";
+    public static final String PATH_FLAGS = PATH_DATA + "flags/";
     public static final String PATH_IMAGES = "./images/";
     public static final String PATH_CSS = "wb/css/";
     public static final String PATH_SITES = "sites/";
